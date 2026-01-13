6b73d065c88edbf5cfddec94e85ce237205a06e8
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-01-13 19:45:01 +0530

    Searching

--- binarySearch.java
+++ binarySearch.java
@@ -1,3 +1,54 @@
-public class binarySearch {
-  
+import java.util.Scanner;
+
+class binarySearch {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;   // target found
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // target not found
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        // Input array elements (sorted)
+        System.out.println("Enter sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Input target
+        System.out.print("Enter target element: ");
+        int target = sc.nextInt();
+
+        // Call binary search
+        int result = search(nums, target);
+
+        // Output result
+        if (result != -1) {
+            System.out.println("Target found at index: " + result);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        sc.close();
+    }
 }
