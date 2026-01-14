3e1b08dd426dc629e5ce83c883c095da538010f5
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-01-14 22:03:23 +0530

    subsequence

---
+++ IsSubsequence.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class IsSubsequence {
+
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string s: ");
+        String s = sc.nextLine();
+
+        System.out.print("Enter string t: ");
+        String t = sc.nextLine();
+
+        boolean result = isSubsequence(s, t);
+
+        System.out.println("It is a subsquence of t" + result);
+
+        sc.close();
+    }
+}
