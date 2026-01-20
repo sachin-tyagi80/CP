2a1f0df68d922d18d790d456a9c43736cd292905
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-01-20 10:05:29 +0530

    insertionSort

---
+++ insertionSort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class insertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take array size
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Take array elements
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Insertion Sort logic (no separate function)
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        // Print sorted array
+        System.out.println("Sorted array:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+
+        sc.close();
+    }
+}
