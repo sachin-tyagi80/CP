e252030cfad0264cdc03325900e1b428b8533b01
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-02-07 22:21:30 +0530

    Rotate Image

---
+++ rotateImage.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class rotateImage {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of matrix
+        System.out.print("Enter size of matrix (n): ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        // Input matrix elements
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        // Print rotated matrix
+        System.out.println("Rotated Matrix (90 degree clockwise):");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
